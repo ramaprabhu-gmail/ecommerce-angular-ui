6cc4b08a0acf26486d96d1d6b56891f1fed225e4
ramaprabhu-gmail <ramaprabhur@gmail.com>
2023-11-27 10:04:22 +0530

    checkout - added controls for customer info and onSubmit event handler

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -9,7 +9,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 
 export class CheckoutComponent implements OnInit {
 
-  checkoutFormGroup: FormGroup | undefined ;
+  checkoutFormGroup!: FormGroup;
 
   constructor(private formBuilder:FormBuilder){}
   ngOnInit(): void {
@@ -20,5 +20,11 @@ export class CheckoutComponent implements OnInit {
         email: ['']
       })
     });
+  }
+
+  onSubmit(){
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log(this.checkoutFormGroup.get('customer')?.value.email);
   }
 }
