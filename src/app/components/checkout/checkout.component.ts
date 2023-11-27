f1f4afb7050f878ae122d8a02cda471e2cc67ce9
ramaprabhu-gmail <ramaprabhur@gmail.com>
2023-11-27 13:05:56 +0530

    checkout - handle months based on year selected

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -89,5 +89,27 @@ export class CheckoutComponent implements OnInit {
     console.log(this.checkoutFormGroup.get('customer')?.value.email);
   }
 
+  handleMonthsAndYears(){
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear); 
+
+    // if the currentYear equals the selectedYear then start with current month
+    let startMonth: number;
+
+    if(currentYear === selectedYear){
+      startMonth = new Date().getMonth() + 1;
+    }else{
+      startMonth = 1;
+    }
+
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log('Retrieved credit card months:' + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
   
 }
