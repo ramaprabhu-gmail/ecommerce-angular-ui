f349ee6c12d02edafe2ebb8d2bba89f51af60a42
ramaprabhu-gmail <ramaprabhur@gmail.com>
2023-11-27 21:03:46 +0530

    checkout load countries into dropdown list

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
@@ -17,6 +19,8 @@ export class CheckoutComponent implements OnInit {
 
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
 
   constructor(private formBuilder:FormBuilder,
               private luv2ShopFormService:Luv2ShopFormService){}
@@ -68,6 +72,14 @@ export class CheckoutComponent implements OnInit {
       data => {
         console.log('Retrieved credit card years:' + JSON.stringify(data));
         this.creditCardYears = data;
+      }
+    );
+
+    // populate countries
+    this.luv2ShopFormService.getCountries().subscribe(
+      data => {
+        console.log('Retrieved countries:' + JSON.stringify(data))
+        this.countries = data
       }
     );
 
